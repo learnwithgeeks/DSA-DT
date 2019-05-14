bd11c45864d58818af0f5c887cd55b3d9d4633d0
viveksharmaui <sharma_vivek62@yahoo.com>
2019-05-14 23:02:26 +0500

    LinkedList

--- Data Structures/linkedList.js
+++ Data Structures/linkedList.js
@@ -11,12 +11,51 @@ function Node(value, next, prev) {
   this.prev = prev;
 }
 
-// Object of Linked List Contructor Function
-const linkedList = new LinkedList();
+// Adding New Node To Head
+LinkedList.prototype.addToHead = function(value) {
+  let newNode = new Node(value, this.head, null);
+  if (!this.head) this.tail = newNode;
+  else this.head.prev = newNode;
+  this.head = newNode;
+};
 
-console.log("Linked List", linkedList);
+// Adding New Node To Tail
+LinkedList.prototype.addToTail = function(value) {
+  let newNode = new Node(value, null, this.tail);
+  if (!this.tail) this.head = newNode;
+  else this.tail.next = newNode;
+  this.tail = newNode;
+};
 
-// Object of Node Constructor Function
-const node = new Node(200, "node2", null);
+// Removing Head
+LinkedList.prototype.removeHead = function() {
+  if (!this.head) return null;
+  this.head = this.head.next;
+  if (this.head) this.head.prev = null;
+  else this.tail = null;
+};
 
-console.log("Node", node);
+// Removing Tail
+LinkedList.prototype.removeTail = function() {
+  if (!this.tail) return null;
+  this.tail = this.tail.prev;
+  if (this.tail) this.tail.next = null;
+  else this.head = null;
+};
+
+// Linked List Object
+let linkedList = new LinkedList();
+
+// Add To Head
+linkedList.addToHead(10);
+
+// Add To Tail
+linkedList.addToTail(20);
+
+// Remove Head
+linkedList.removeHead();
+
+// Remove Tail
+linkedList.removeTail();
+
+console.log(linkedList);
